263749cbbfe8feec07461dd783447a114f03daec
Starskii <jakeylakie@yahoo.com>
2020-12-04 04:14:04 -0500

    bumped up speed because avenger bonus

--- Champions.py
+++ Champions.py
@@ -94,22 +94,22 @@ class CaptainAmerica(Champion):
         self._range = 5
         if self._click_value is 1:
             self._KO = False
-            self._speed = 8
+            self._speed = 9
             self._attack = 11
             self._defense = 17
             self._damage = 3
         elif 1 < self._click_value < 4:
-            self._speed = 7
+            self._speed = 8
             self._attack = 10
             self._defense = 17
             self._damage = 3
         elif 3 < self._click_value < 6:
-            self._speed = 6
+            self._speed = 7
             self._attack = 9
             self._defense = 16
             self._damage = 2
         else:
-            self._speed = 5
+            self._speed = 6
             self._attack = 9
             self._defense = 17
             self._damage = 2
@@ -122,22 +122,22 @@ class IronMan(Champion):
         self._range = 7
         if self._click_value is 1:
             self._KO = False
-            self._speed = 10
+            self._speed = 11
             self._attack = 10
             self._defense = 18
             self._damage = 4
         elif 1 < self._click_value < 4:
-            self._speed = 10
+            self._speed = 11
             self._attack = 10
             self._defense = 17
             self._damage = 3
         elif 3 < self._click_value < 6:
-            self._speed = 9
+            self._speed = 10
             self._attack = 9
             self._defense = 17
             self._damage = 2
         else:
-            self._speed = 8
+            self._speed = 9
             self._attack = 9
             self._defense = 16
             self._damage = 2
@@ -150,27 +150,27 @@ class Thor(Champion):
         self._range = 6
         if self._click_value is 1:
             self._KO = False
-            self._speed = 10
+            self._speed = 11
             self._attack = 11
             self._defense = 18
             self._damage = 4
         elif 1 < self._click_value < 4:
-            self._speed = 10
+            self._speed = 11
             self._attack = 11
             self._defense = 17
             self._damage = 4
         elif 3 < self._click_value < 7:
-            self._speed = 10
+            self._speed = 11
             self._attack = 10
             self._defense = 17
             self._damage = 3
         elif 6 < self._click_value < 9:
-            self._speed = 9
+            self._speed = 10
             self._attack = 9
             self._defense = 17
             self._damage = 3
         else:
-            self._speed = 9
+            self._speed = 10
             self._attack = 9
             self._defense = 16
             self._damage = 3
